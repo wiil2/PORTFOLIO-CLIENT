3d7a8cf2595a66365aea24386d730d3d993a5ba6
NicholasLeao <tropicaldata@gmail.com>
2022-07-08 17:19:09 -0300

    mudei o handleSubmit

--- src/Pages/ProfileEdit/index.js
+++ src/Pages/ProfileEdit/index.js
@@ -4,323 +4,344 @@ import { api } from "../../api/api";
 import { AuthContext } from "../../contexts/authContext";
 import styled from "styled-components";
 
-
 export function ProfileEdit() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [form, setForm] = useState({
-        name: "",
-        email: "",
-        citystate: "",
-        phone: "",
-        age: "",
-        denomination: "",
-        interests:"",
-        description: "",
-        tags: "",
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    citystate: "",
+    phone: "",
+    age: "",
+    denomination: "",
+    interests: "",
+    description: "",
+    tags: "",
+  });
+
+  const { loggedInUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    async function fetchCadastro() {
+      const response = await api.get("/user/profile");
+      setForm({ ...response.data });
+    }
+    // *** VERIFICAR SE HOUVE ERRO
+    fetchCadastro().catch((err) => console.log(err));
+  }, []);
+
+  function handleChange(e) {
+    // *** UTILIZAR O ESTADO PREVIO DO STATE PASSANDO UMA CALLBACK E RETORNANDO NOVO VALOR
+    setForm((prevForm) => {
+      return { ...prevForm, [e.target.name]: e.target.value };
     });
+  }
 
-    const { loggedInUser } = useContext(AuthContext);
+  function handleSubmit(e) {
+    e.preventDefault();
 
-    useEffect(() => {
-        async function fetchCadastro() {
-            const response = await api.get("/user/profile");
-            setForm({ ...response.data })
-        }
-        fetchCadastro();
-    }, []);
+    // *** CHECAR SE OF FORM TA CERTO ANTES DE MANDAR
+    if (!form) return console.log("***FORMULARIO UNDEFINED***");
 
-    function handleChange(e) {
-        setForm({ ...form, [e.target.name]: e.target.value });
-    }
+    (async () => {
+      try {
+        await api.patch("/user/update-profile", { ...form });
+        navigate("/profile");
+      } catch (err) {
+        console.log("***ERRO DO PATCH***", err);
+      }
+    })();
+  }
 
-    async function handleSubmit(e) {
-        e.preventDefault();
+  const formId = form._id;
 
-        try{
+  function deleteUser() {
+    api.delete("/user/delete-user", { formId });
+    navigate("/");
+    return;
+  }
 
-            await api.patch("/user/update-profile", { ...form });
-            navigate("/profile")
-
-        } catch(err) {
-            console.log(err)
-        }
-    }
-
-
-    const formId = form._id
-
-    function deleteUser() {
-        api.delete("/user/delete-user", {formId});
-        navigate("/");
-        return
-    }
-
-
-
- return (  
-  <>
-    <SContainer>
+  return (
+    <>
+      <SContainer>
         <h1>editar perfil</h1>
 
-            <SMiddle>
+        <SMiddle>
+          <form onSubmit={handleSubmit}>
+            <div className="forms1">
+              <input
+                id="formName"
+                name="name"
+                type="text"
+                value={form.name}
+                onChange={handleChange}
+                placeholder="Your Name"
+              />
+              <input
+                id="formEmail"
+                name="email"
+                type="email"
+                value={form.email}
+                onChange={handleChange}
+                placeholder="Your E-mail"
+              />
+              <input
+                id="formCityState"
+                name="citystate"
+                type="text"
+                value={form.citystate}
+                onChange={handleChange}
+                placeholder="Your City And State"
+              />
+              <input
+                id="formPhone"
+                name="Phone"
+                type="text"
+                value={form.phone}
+                onChange={handleChange}
+                placeholder="Your Phone"
+              />
+              <input
+                id="formAge"
+                name="Age"
+                type="text"
+                value={form.age}
+                onChange={handleChange}
+                placeholder="Your Age"
+              />
+            </div>
+            <div className="forms2">
+              <input
+                id="formDenomination"
+                name="Denomination"
+                type="text"
+                value={form.denomination}
+                onChange={handleChange}
+                placeholder="Your Denomination"
+              />
+              <input
+                id="formInterests"
+                name="Interests"
+                type="text"
+                value={form.interests}
+                onChange={handleChange}
+                placeholder="Your Interests"
+              />
+            </div>
+            <div className="forms3">
+              <textarea
+                id="formDescription"
+                name="Description"
+                type="text"
+                value={form.description}
+                onChange={handleChange}
+                placeholder="Your Description"
+              />
+            </div>
+            <div className="selects">
+              <h3>Redes Sociais</h3>
+              <div>
+                <select onClick={handleChange} name="tags">
+                  <option value="Instagram">Instagram</option>
+                  <option value="Linkedin">Linkedin</option>
+                  <option value="Github">GitHub</option>
+                  <option value="WhatsApp">WhatsApp</option>
+                </select>
+                <input
+                  id="formTags"
+                  name="tags"
+                  type="text"
+                  value={form.tags}
+                  onChange={handleChange}
+                  placeholder="insira o link aqui"
+                />
+              </div>
+              <div>
+                <select onClick={handleChange} name="tags">
+                  <option value="Instagram">Instagram</option>
+                  <option value="Linkedin">Linkedin</option>
+                  <option value="Github">GitHub</option>
+                  <option value="WhatsApp">WhatsApp</option>
+                </select>
+                <input
+                  id="formTags"
+                  name="tags"
+                  type="text"
+                  value={form.tags}
+                  onChange={handleChange}
+                  placeholder="insira o link aqui"
+                />
+              </div>
+              <div>
+                <select onClick={handleChange} name="tags">
+                  <option value="Instagram">Instagram</option>
+                  <option value="Linkedin">Linkedin</option>
+                  <option value="Github">GitHub</option>
+                  <option value="WhatsApp">WhatsApp</option>
+                </select>
+                <input
+                  id="formTags"
+                  name="tags"
+                  type="text"
+                  value={form.tags}
+                  onChange={handleChange}
+                  placeholder="insira o link aqui"
+                />
+              </div>
+              <div>
+                <select onClick={handleChange} name="tags">
+                  <option value="Instagram">Instagram</option>
+                  <option value="Linkedin">Linkedin</option>
+                  <option value="Github">GitHub</option>
+                  <option value="WhatsApp">WhatsApp</option>
+                </select>
+                <input
+                  id="formTags"
+                  name="tags"
+                  type="text"
+                  value={form.tags}
+                  onChange={handleChange}
+                  placeholder="insira o link aqui"
+                />
+              </div>
+            </div>
+            <div className="buttons">
+              <button type="submit">CONFIRM</button>
+              <button onClick={deleteUser}>DELETE PROFILE</button>
+            </div>
+          </form>
 
-                <form onSubmit={
-                    handleSubmit}>
-                    <div className="forms1">
-                        <input id="formName"    
-                            name="name"
-                            type="text" 
-                            value={form.name}
-                            onChange={handleChange} 
-                            placeholder="Your Name" 
-                        />
-                        <input id="formEmail"
-                            name="email"
-                            type="email" 
-                            value={form.email}
-                            onChange={handleChange} 
-                            placeholder="Your E-mail" 
-                        />
-                        <input id="formCityState"
-                            name="citystate"
-                            type="text" 
-                            value={form.citystate}
-                            onChange={handleChange} 
-                            placeholder="Your City And State" 
-                        />
-                        <input id="formPhone"
-                            name="Phone"
-                            type="text" 
-                            value={form.phone}
-                            onChange={handleChange} 
-                            placeholder="Your Phone" 
-                        />
-                            <input id="formAge"
-                            name="Age"
-                            type="text" 
-                            value={form.age}
-                            onChange={handleChange} 
-                            placeholder="Your Age" 
-                        />
-                    </div>
-                    <div className="forms2">
-                        <input id="formDenomination"
-                            name="Denomination"
-                            type="text" 
-                            value={form.denomination}
-                            onChange={handleChange} 
-                            placeholder="Your Denomination" 
-                        />
-                        <input id="formInterests"
-                            name="Interests"
-                            type="text" 
-                            value={form.interests}
-                            onChange={handleChange} 
-                            placeholder="Your Interests" 
-                        />
-                    </div>
-                    <div className="forms3">
-                        <textarea id="formDescription"
-                            name="Description"
-                            type="text" 
-                            value={form.description}
-                            onChange={handleChange} 
-                            placeholder="Your Description" 
-                        />
-                    </div>
-                     <div className="selects">
-                        <h3>Redes Sociais</h3>
-                        <div>
-                            <select onClick={handleChange} name="tags">
-                                <option  value="Instagram">Instagram</option>
-                                <option  value="Linkedin">Linkedin</option>
-                                <option  value="Github">GitHub</option>
-                                <option  value="WhatsApp">WhatsApp</option>
-                            </select>
-                            <input id="formTags" name="tags" type="text" value={form.tags} onChange={handleChange} placeholder="insira o link aqui"/>
-                        </div>
-                        <div>
-                            <select onClick={handleChange} name="tags">
-                                <option  value="Instagram">Instagram</option>
-                                <option  value="Linkedin">Linkedin</option>
-                                <option  value="Github">GitHub</option>
-                                <option  value="WhatsApp">WhatsApp</option>
-                            </select>
-                            <input id="formTags" name="tags" type="text" value={form.tags} onChange={handleChange}  placeholder="insira o link aqui"/>
-                        </div>
-                        <div>
-                            <select onClick={handleChange} name="tags">
-                                <option  value="Instagram">Instagram</option>
-                                <option  value="Linkedin">Linkedin</option>
-                                <option  value="Github">GitHub</option>
-                                <option  value="WhatsApp">WhatsApp</option>
-                            </select>
-                            <input id="formTags" name="tags" type="text" value={form.tags} onChange={handleChange} placeholder="insira o link aqui"/>
-                        </div>
-                        <div>
-                            <select onClick={handleChange} name="tags">
-                                <option  value="Instagram">Instagram</option>
-                                <option  value="Linkedin">Linkedin</option>
-                                <option  value="Github">GitHub</option>
-                                <option  value="WhatsApp">WhatsApp</option>
-                            </select>
-                            <input id="formTags" name="tags" type="text" value={form.tags} onChange={handleChange} placeholder="insira o link aqui"/>
-                        </div>
-                    </div>
-                    <div className="buttons">
-                    <button type="submit">CONFIRM</button>
-                    <button onClick={deleteUser}>DELETE PROFILE</button>
-                </div>
-
-                </form>
-
-                <div className="photo">
-                    <img src={loggedInUser.user.img} alt={loggedInUser.user.name} />
-                    <button>Change Photo</button>
-                </div>
-
-                
-
-            </SMiddle>
-
-                
-            
-    </SContainer>
-  
-  </>
-
-
-    );
+          <div className="photo">
+            <img src={loggedInUser.user.img} alt={loggedInUser.user.name} />
+            <button>Change Photo</button>
+          </div>
+        </SMiddle>
+      </SContainer>
+    </>
+  );
 }
 
 export default ProfileEdit;
 
-// =========================== STYLES ============================= // 
+// =========================== STYLES ============================= //
 
 const SContainer = styled.div`
-& h1 {
+  & h1 {
     text-decoration: overline;
-    text-decoration-color: #FF004F;
-    font-family: 'Mukta';
-    color: #FAEAA7;
+    text-decoration-color: #ff004f;
+    font-family: "Mukta";
+    color: #faeaa7;
     font-size: 90px;
     margin-left: 40px;
-}
-& .buttons {
+  }
+  & .buttons {
     text-align: center;
-}
-& .buttons button {
+  }
+  & .buttons button {
     border: 3px solid white;
-    color: #FAEAA7;
-    background-color: #14202E ;
-    border: 1px solid #14202E;
+    color: #faeaa7;
+    background-color: #14202e;
+    border: 1px solid #14202e;
     align-items: center;
     border-radius: 5px;
-    font-family: 'Mukta';
+    font-family: "Mukta";
     font-size: 20px;
     margin: 3px;
     margin-top: 25px;
-    
-    
-}
-`
+  }
+`;
 
 const SMiddle = styled.div`
-display: flex;
-justify-content: space-around;
-//border: 5px solid red;
-& .forms1 {
+  display: flex;
+  justify-content: space-around;
+  //border: 5px solid red;
+  & .forms1 {
     //border: 3px solid green;
-    
-    
-}
-& .forms1 input {
+  }
+  & .forms1 input {
     width: 140px;
     margin: 2px;
     height: 30px;
     font-size: 20px;
-    border: 1px solid #14202E;
-    background-color: #14202E;
-    color: #FAEAA7;
-    font-family: 'Gantari';
+    border: 1px solid #14202e;
+    background-color: #14202e;
+    color: #faeaa7;
+    font-family: "Gantari";
     padding-left: 15px;
     //border-radius: 15px;
-}
-& .forms2 {
+  }
+  & .forms2 {
     //border: 3px solid yellow;
-    
-}
-& .forms2 input {
+  }
+  & .forms2 input {
     width: 384px;
     height: 30px;
     margin: 2px;
     font-size: 20px;
-    border: 1px solid #14202E;
-    background-color: #14202E;
-    color: #FAEAA7;
-    font-family: 'Gantari';
+    border: 1px solid #14202e;
+    background-color: #14202e;
+    color: #faeaa7;
+    font-family: "Gantari";
     padding-left: 15px;
-}
-& .forms3 {
+  }
+  & .forms3 {
     //border: 3px solid pink;
-}
-& .forms3 textarea {
+  }
+  & .forms3 textarea {
     width: 790px;
     height: 150px;
     margin: 2px;
     font-size: 20px;
-    border: 1px solid #14202E;
-    background-color: #14202E;
-    color: #FAEAA7;
-    font-family: 'Gantari';
+    border: 1px solid #14202e;
+    background-color: #14202e;
+    color: #faeaa7;
+    font-family: "Gantari";
     padding-left: 15px;
     resize: none;
-}
-& .selects {
+  }
+  & .selects {
     //border: 3px solid orange;
-    
-}
-& .selects h3 {
-    font-family: 'Mukta';
-    color: #FAEAA7;
+  }
+  & .selects h3 {
+    font-family: "Mukta";
+    color: #faeaa7;
     margin-bottom: 5px;
-}
-& .selects select {
-    font-family: 'Mukta';
-    color: #FAEAA7;
-    background-color: #14202E ;
-    border: 1px solid #14202E;
+  }
+  & .selects select {
+    font-family: "Mukta";
+    color: #faeaa7;
+    background-color: #14202e;
+    border: 1px solid #14202e;
     margin: 3px;
     height: 40px;
-}
-& .selects input {
-    color: #FAEAA7;
-    background-color: #14202E ;
-    border: 1px solid #14202E;
+  }
+  & .selects input {
+    color: #faeaa7;
+    background-color: #14202e;
+    border: 1px solid #14202e;
     height: 20px;
     align-items: center;
     height: 30px;
     width: 600px;
-}
-& .photo {
+  }
+  & .photo {
     //border: 3px solid gray;
-    display:flex;
+    display: flex;
     flex-direction: column;
-}
-& .photo img {
+  }
+  & .photo img {
     border-radius: 2000px;
     width: 350px;
     margin-bottom: 10px;
-}
-& .photo button {
-    color: #FAEAA7;
-    background-color: #14202E ;
-    border: 1px solid #14202E;
+  }
+  & .photo button {
+    color: #faeaa7;
+    background-color: #14202e;
+    border: 1px solid #14202e;
     align-items: center;
     border-radius: 5px;
     padding: 5px 70px;
-    font-family: 'Mukta';
+    font-family: "Mukta";
     font-size: 20px;
-}
-`
+  }
+`;
