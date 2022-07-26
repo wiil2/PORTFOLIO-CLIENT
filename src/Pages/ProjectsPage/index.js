f9bbb2db109436e3bf53ac1b23a9da738f7766f6
NicholasLeao <tropicaldata@gmail.com>
2022-07-26 10:51:23 -0300

    Typo na url do request na pagina ProjectsPage

--- src/Pages/ProjectsPage/index.js
+++ src/Pages/ProjectsPage/index.js
@@ -1,39 +1,29 @@
-import {useParams} from "react-router-dom";
-import {api} from "../../api/api";
+import { useParams } from "react-router-dom";
+import { api } from "../../api/api";
 import { useEffect, useState, useContext } from "react";
 import { AuthContext } from "../../contexts/authContext";
 import styled from "styled-components";
 
-
 export function ProjectPage() {
+  const [projects, setProjects] = useState([]);
+  const { loggedInUser } = useContext(AuthContext);
+  //const navigate = useNavigate();
+  const { id } = useParams();
 
-    const [projects, setProjects] = useState([]);
-    const {loggedInUser} = useContext(AuthContext);
-    //const navigate = useNavigate();
-    const { id } = useParams();
+  useEffect(() => {
+    async function fetchProjects() {
+      const response = await api.get(`/projects/projects/${id}`);
+      setProjects(response.data);
+      console.log(response.data);
+    }
+    fetchProjects();
+  });
 
-
-    useEffect (() => {
-        async function fetchProjects() {
-            const response = await api.get(`/projects/${id}`);
-            setProjects(response.data);
-            console.log(response.data);
-        }
-        fetchProjects();
-    }); 
-
-
-    return ( 
-        <>
-            <h1>PROJETOS</h1>
-        
-        
-        
-        </>
-
-
-
-    )
+  return (
+    <>
+      <h1>PROJETOS</h1>
+    </>
+  );
 }
 
 export default ProjectPage;
